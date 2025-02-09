af21d8ba943e594ebfaf4aee48ca672b279303a9
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2025-02-09 20:16:55 +0900

    create function on calculate and  move code in there

--- interest_calculator.js
+++ interest_calculator.js
@@ -1,3 +1,7 @@
 //Formula: A = P(1 + r/n)nt</sup>sup> + PMT((1 + r/n)nt - 1)/(r/n)
 const btn = document.getElementsById("calculate-button");
 btn.addEventListener("click", calculate);
+
+function calculate(){
+    //Formula: A = P(1 + r/n)nt</sup>sup> + PMT((1 + r/n)nt - 1)/(r/n)
+}
