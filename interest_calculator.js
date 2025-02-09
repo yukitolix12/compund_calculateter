e0f107e9a1d1bbb035777855b7bc0552c90e0342
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2025-02-09 20:20:22 +0900

    make input value to set P

--- interest_calculator.js
+++ interest_calculator.js
@@ -4,4 +4,5 @@ btn.addEventListener("click", calculate);
 
 function calculate(){
     //Formula: A = P(1 + r/n)nt</sup>sup> + PMT((1 + r/n)nt - 1)/(r/n)
+    let P = Number(document.getElementById("Starting-balance").value);
 }
