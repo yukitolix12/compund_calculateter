9c2e0da719b9abc9e0891da4c3c49568c2dea635
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2025-02-10 20:59:06 +0900

    set n for make the compound frequency

--- interest_calculator.js
+++ interest_calculator.js
@@ -6,4 +6,5 @@ function calculate(){
     //Formula: A = P(1 + r/n)nt</sup>sup> + PMT((1 + r/n)nt - 1)/(r/n)
     let P = Number(document.getElementById("Starting-balance").value);
     let r = Number(document.getElementById("Interest-rate").value)/100;
+    let n = Number(document.getElementById("compound").value);
 }
