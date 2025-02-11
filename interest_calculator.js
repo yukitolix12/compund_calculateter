451d8f7f915c84b1138547cb82483542f18298c4
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2025-02-11 21:12:02 +0900

    fix typoes for run program

--- interest_calculator.js
+++ interest_calculator.js
@@ -1,11 +1,11 @@
 //Formula: A = P(1 + r/n)nt</sup>sup> + PMT((1 + r/n)nt - 1)/(r/n)
-const btn = document.getElementsById("calculate-button");
+const btn = document.getElementById("calculate-button");
 btn.addEventListener("click", calculate);
 
 function calculate(){
     //Formula: A = P(1 + r/n)nt</sup>sup> + PMT((1 + r/n)nt - 1)/(r/n)
-    let P = Number(document.getElementById("Starting-balance").value);
-    let r = Number(document.getElementById("Interest-rate").value)/100;
+    let P = Number(document.getElementById("starting-balance").value);
+    let r = Number(document.getElementById("interest-rate").value)/100;
     let n = Number(document.getElementById("compound").value);
     let t = Number(document.getElementById("years").value);
     let PMT = Number(document.getElementById("contribution").value);
