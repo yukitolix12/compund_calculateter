cbcf894b718524e592e36fdfa12e4835d4dca345
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2025-02-12 20:14:17 +0900

    copy and paste to codes from chart.js again

--- interest_calculator.js
+++ interest_calculator.js
@@ -15,4 +15,25 @@ function calculate(){
     let result2 = PMT*(temp - 1)/(r/n);
     let result = (result1 + result2).toFixed(2);
     document.getElementById("result").innerText = result;
+
+    const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
 }
